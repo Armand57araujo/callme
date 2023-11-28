9abc00e6c3fc5725cb7f28ee3e844f3ae4ab5ef8
James Hunter <jamessahunter@gmail.com>
2023-11-28 11:40:59 -0800

    add db init and put

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -3,12 +3,28 @@ import { openDB } from 'idb';
 
 // TODO: Complete the initDb() function below:
 const initdb = async () => {
-
-};
+    openDB('contacts', 1, {
+        upgrade(db) {
+          if (db.objectStoreNames.contains('contacts')) {
+            console.log('contacts database already exists');
+            return;
+          }
+          db.createObjectStore('contacts', { keyPath: 'id', autoIncrement: true });
+          console.log('contacts database created');
+        },
+      }
+)};
 
 
 // TODO: Complete the postDb() function below:
 export const postDb = async (name, home, cell, email)  => {
+    console.log('Post to the database');
+    const contactsDb = await openDB('contacts', 1);
+    const tx = contactsDb.transaction('contacts', 'readwrite');
+    const store = tx.objectStore('contacts');
+    const request = store.add({ name, home, cell, email });
+    const result = await request;
+    console.log('Data saved to the database', result);
  
 };
 
