5e5720e7dc7184b131b37c1dc9f86ef5950a6a55
FIRST_NAME LAST_NAME <armand_araujo@yahoo.com>
2023-11-28 11:31:45 -0800

    completed src-sw.js file

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -1,1 +1,55 @@
 // TODO: Create a service worker that caches static assets:
+
+const staticCacheName = 'site-static-v1';
+const dynamicCacheName = 'site-dynamic-v1';
+
+const assetsToCache = [
+    '/',
+    '/index.html',
+    '/install.html',
+    '/cards.html',
+    '/src/css/style.css',
+    '/src/js/index.js',
+    '/src/js/install.js',
+    '/src/js/cards.js',
+    '/src/js/app.js',
+];
+
+self.addEventListener('install', evt => {
+    // console.log('service worker has been installed');
+    evt.waitUntil(
+        caches.open(staticCacheName).then(cache => {
+            console.log('caching shell assets');
+            cache.addAll(assetsToCache);
+        })
+    );
+});
+
+self.addEventListener('activate', evt => {
+    // console.log('service worker has been activated');
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            // console.log(keys);
+            return Promise.all(keys
+                .filter(key => key !== staticCacheName && key !== dynamicCacheName)
+                .map(key => caches.delete(key))
+            )
+        })
+    );
+});
+
+self.addEventListener('fetch', evt => {
+    // console.log('fetch event', evt);
+    evt.respondWith(
+        caches.match(evt.request).then(cacheRes => {
+            return cacheRes || fetch(evt.request).then(fetchRes => {
+                return caches.open(dynamicCacheName).then(cache => {
+                    cache.put(evt.request.url, fetchRes.clone());
+                    return fetchRes;
+                })
+            });
+        })
+    );
+});
+
+
